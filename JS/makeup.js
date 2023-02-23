4b1e75faabeb95be8067b332f2e12da411e01062
abhithemauryas <abhithemauryas5@gmail.com>
2023-02-24 01:00:41 +0530

    day-3 complete

---
+++ JS/makeup.js
@@ -0,0 +1,28 @@
+fetch("https://63f7075ae40e087c95861b07.mockapi.io/myGlmm")
+.then((resObj)=>{
+    return resObj.json();
+})
+.then((accData)=>{
+    console.log(accData.data)
+    displayProduct(accData.data)
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+let container =document.getElementById("api")
+function displayProduct(data){
+    data.forEach((element)=>{
+        let card =document.createElement("div")
+        let image=document.createElement("img")
+        image.setAttribute("src",element.img);
+        let desc=document.createElement("p")
+        desc.innerText=element.desc
+
+
+
+
+        card.append(image,desc)
+        container.append(api)
+    })
+}
